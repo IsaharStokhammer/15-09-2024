fd811d00eea72246baab42075eca1539a1539290
IsaharStokhammer <8526656@gmail.com>
2024-09-15 12:16:39 +0300

    jhgfd

--- main.js
+++ main.js
@@ -1,10 +1,55 @@
-function getData(callback) {
-    let data = 0;
-    setTimeout(() => {
-        data = 'data';
-        callback(data);
-    }, 1000);
+function Mission1(){
+    return new Promise((resolve, reject) => {
+        resolve("Success!");
+        reject("Failure!");
+    })
 }
-function printData(data){console.log(data)};
+Mission1().then((res)=> console.log(res)).catch((err) => console.log(err));
 
-getData(printData);
+function Mission2(firstName, lastName) {
+    let fullName = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(firstName + " " + lastName);
+        }, 1000);
+    });
+
+    return fullName;
+}
+
+
+Mission2("John", "Doe").then(result => {
+    console.log(result);
+});
+
+
+function multiplyByTwo(number) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(number * 2);
+        }, 500);
+    });
+}
+
+function addFive(number) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(number + 5);
+        }, 500);
+    });
+}
+
+function subtractTen(number) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(number - 10);
+        }, 500);
+    });
+}
+
+
+multiplyByTwo(5)
+    .then(result => addFive(result))
+    .then(result => subtractTen(result))
+    .then(finalResult => {
+        console.log(finalResult); 
+    });
